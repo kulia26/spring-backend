a3a5ed443b6aaa8082c6f30f14e854350608cf52
kulia26 <kulinich261@gmail.com>
2019-12-13 16:35:43 +0200

    added files

---
+++ src/main/java/lab/Main.java
@@ -0,0 +1,20 @@
+
+package lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Collections;
+
+@ComponentScan
+@EnableAutoConfiguration
+@EnableWebMvc
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Main.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8000"));
+        app.run(args);
+    }
+}
